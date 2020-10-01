4b90d241f3d09a2bee5618d0ae6bf7cbe650ccc8
Lurein Perera <pererag@bc.edu>
2020-09-30 23:53:44 -0400

    added bar charts

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
     var filteredCities = data;
     cities = cities.filter(city => city.eu === true);
     console.log('cities', cities);
-    d3.select('.city-count').text(cities.length);
+    d3.select('.city-count').text(cities.length + " cities");
     const width = 700;
     const height = 550;
     const svg = d3.select('.population-plot')
@@ -47,3 +47,44 @@ d3.csv('cities.csv', d3.autoType).then(data=>{
             .attr("text-anchor", "middle")
             .attr("dx", "30")
 })
+
+var buildings;
+d3.csv('buildings.csv', d3.autoType).then(data=>{ 
+    console.log(data)
+    var buildings = data;
+    buildings.sort(function(a, b) {
+		return parseFloat(b.height_ft) - parseFloat(a.height_ft);
+	});
+    const barChartWidth = 500;
+    const barChartHeight = 500;
+    const barSVG = d3.select('.bar-chart')
+            .append('svg')
+            .attr('width', barChartWidth)
+            .attr('height', barChartHeight)
+            .append("g")
+    barSVG.append('g')
+        .selectAll('bar')
+        .data(buildings)
+        .enter()
+        .append("rect")
+            .attr("width", "70")
+            .attr("height", function (d) { return d.height_px; } )
+            .attr("x", function (d, i) { 
+                return (i * 70); 
+            } )
+            .attr("y", function (d) { return 500 - d.height_px; } )
+    barSVG.append('g')
+            .selectAll("labels")
+            .data(buildings)
+            .enter()
+            .append("text")
+                .attr("x", function (d, i) { 
+                    return (i * 70); 
+                } )
+                .attr("y", function (d) { return 500 - d.height_px; } )
+                .text(function (d) { return d.building; } )
+                .attr("font-size", "11px")
+                .attr("dy", "-15")
+})
+
+
